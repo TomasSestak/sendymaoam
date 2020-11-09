cbc99860ab9f8139fdef2d66d8871d53b1e48196
Tomáš Šesták <t.sestak@sofistai.cz>
2020-11-09 23:05:18 +0100

    improve db

--- config/database.js
+++ config/database.js
@@ -2,14 +2,13 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: 'mongoose',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        uri: 'mongodb://sendy:Mamradavino7@cluster0-shard-00-00.nbs54.mongodb.net:27017,cluster0-shard-00-01.nbs54.mongodb.net:27017,cluster0-shard-00-02.nbs54.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-d9k7cr-shard-0&authSource=admin&retryWrites=true&w=majority'
       },
       options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
+        ssl: true
+      }
+    }
+  }
 });
