78c0d2f76532c238fafe96e5bf0317bd1ccbecfb
Tomáš Šesták <t.sestak@sofistai.cz>
2021-02-14 18:44:34 +0100

    remove endpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,5 @@
-import useTranslation from 'next-translate/useTranslation';
-import Typo from '@/components/core/Typo';
-import Icon from '@/components/core/Icon';
-import {FunctionComponent, ReactComponentElement, ReactElement, useState} from 'react';
-import { Translate } from 'next-translate';
-import Button from '@/components/core/Button';
-import Link from 'next/link';
+import {FunctionComponent, ReactElement} from 'react';
 import {NextRouter, useRouter} from 'next/router';
-import AuthorizationBox from '@/components/authorization/AuthorizationBox';
-import Login from '@/components/authorization/Login';
-import Activation from '@/components/authorization/Activation';
-import ForgottenPassword from '@/components/authorization/ForgottenPassword';
-import NextImage from 'next/image';
 
 const Index: FunctionComponent = (): ReactElement<'div'> => {
 
