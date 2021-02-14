79648047081e965b5288befb9b834a86d46588fe
Tomáš Šesták <t.sestak@sofistai.cz>
2021-02-14 18:50:04 +0100

    remove redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,19 @@
 import '@/scss/main.scss';
 
-import {Provider} from 'react-redux';
-import {CombinedState, configureStore} from '@reduxjs/toolkit';
-import combinedReducer from '@/store/index';
 import {LayoutTree} from '@moxy/next-layout';
 import Layout from '@/components/layout/Layout';
 import {FunctionComponent, ReactComponentElement} from 'react';
-import Router from 'next/router';
 import {AppProps} from 'next/app';
 
-const reducer = (state, action) => {
-	if (action.type === 'authorization/logout') {
-		state = undefined;
-	}
-	return combinedReducer(state, action);
-};
-
-const store = configureStore({ reducer });
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
 
 const App: FunctionComponent<AppProps> = ({ Component, pageProps }): ReactComponentElement<typeof Provider> => {
 	return (
-		<Provider store={store}>
 			<Layout>
 				<LayoutTree
 					Component={Component}
 					pageProps={pageProps}
 				/>
 			</Layout>
-		</Provider>
 	);
 };
 
