df534646e80549120b4adcf8d98b9a686dbe2b9b
Tomáš Šesták <t.sestak@sofistai.cz>
2021-02-26 20:27:20 +0100

    add GTM module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,20 @@ import 'swiper/swiper.scss';
 import 'swiper/components/pagination/pagination.scss';
 import '@/scss/main.scss';
 
+import TagManager from 'react-gtm-module';
 import Layout from '@/components/layout/Layout';
-import {FunctionComponent, ReactComponentElement} from 'react';
+import {FunctionComponent, ReactComponentElement, useEffect} from 'react';
 import {AppProps} from 'next/app';
 import {SWRConfig} from 'swr'
 
+const tagManagerArgs = {
+	gtmId: 'GTM-5XZ62MN',
+};
+
 const App: FunctionComponent<AppProps> = ({Component, pageProps}): ReactComponentElement<typeof Layout> => {
+	useEffect(() => {
+		TagManager.initialize(tagManagerArgs);
+	}, []);
 	return (
 		<SWRConfig value={{refreshInterval: 60000}}>
 			<Layout>
